c474bbb5de610da9f55e05ec84a3b830226495e3
Shoko Furushita <shktkt219@gmail.com>
2016-05-04 23:25:26 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  name
+
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+end
